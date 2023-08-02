468682bfb27e613326bcc260c20e87b19642687a
vladislav.kubasov <vladislav.kubasov@nordclan.com>
2023-08-02 22:37:48 +0400

    added user conf

---
+++ lua/user/plugins/community.lua
@@ -0,0 +1,9 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.lsp.inc-rename-nvim"},
+  { import = "astrocommunity.utility.transparent-nvim"},
+}
